d7ebdcf254bc174e414037ce07c18f78af099bcf
manchi17 <manseed.rt7@gmail.com>
2022-01-20 15:48:24 +0530

    Added push notification

--- App.js
+++ App.js
@@ -5,8 +5,8 @@
  * @format
  * @flow strict-local
  */
-
-import React from 'react';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import React,{useState,useEffect} from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -20,31 +20,10 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import PushNotification from "react-native-push-notification";
+import Firebase from '@react-native-firebase/app'
+
+
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -52,6 +31,65 @@ const App = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    createChannels()
+  }, [])
+
+
+  const createChannels = () => {
+    Firebase.initializeApp();
+    PushNotification.configure({
+      // (optional) Called when Token is generated (iOS and Android)
+      onRegister: function (token) {
+        console.log("TOKEN:", token);
+      },
+    
+      // (required) Called when a remote is received or opened, or local notification is opened
+      onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+    
+        // process the notification
+    
+        // (required) Called when a remote is received or opened, or local notification is opened
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+    
+      // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+      onAction: function (notification) {
+        console.log("ACTION:", notification.action);
+        console.log("NOTIFICATION:", notification);
+    
+        // process the action
+      },
+    
+      // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+      onRegistrationError: function(err) {
+        console.error(err.message, err);
+      },
+    
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+    
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+    
+      /**
+       * (optional) default: true
+       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+       * - if you are not using remote notification or do not have Firebase installed, use this:
+       *     requestPermissions: Platform.OS === 'ios'
+       */
+      requestPermissions: true,
+    });
+    }
+
 
   return (
     <SafeAreaView style={backgroundStyle}>
